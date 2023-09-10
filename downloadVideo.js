bf1c1e0808cb8e7dc45761c4560e1d7983ba745b
Abdussalam Mujeeb-ur-rahman <103681250+Abdussalam-Mujeeb-ur-rahman@users.noreply.github.com>
2023-09-10 15:38:23 +0100

    ..

--- downloadVideo.js
+++ downloadVideo.js
@@ -1,30 +1,31 @@
-const { execFile } = require('youtube-dl-exec')
+const { exec } = require('youtube-dl-exec');
 const fs = require('fs');
 
-
-
-
 async function download_video(videoUrl, folderName) {
-    if (videoUrl && folderName) {
-        try {
-          const options = {
-            url: videoUrl,
-            output: `${folderName}/%(title)s.%(ext)s`, // Define the output filename
-          };
-    
-          console.log('Download started');
-    
-          // Execute the download
-          await exec(options);
-    
-          console.log('Finished downloading!');
-        } catch (error) {
-          console.error('Error:', error.message);
-        }
-      } else {
-        console.error('Both videoUrl and folderName must be provided.');
-      }
+  if (videoUrl && folderName) {
 
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+  }
+
+    try {
+      const options = {
+        url: videoUrl,
+        output: `${folderName}/%(title)s.%(ext)s`, // Define the output filename
+      };
+
+      console.log('Download started');
+
+      // Execute the download
+      await exec(options);
+
+      console.log('Finished downloading!');
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  } else {
+    console.error('Both videoUrl and folderName must be provided.');
+  }
 }
 
-module.exports = { download_video }
+module.exports = { download_video };
--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 #!/usr/bin/env node
-const { Command } = require("commander");
-const { downloadVideo } = require("./download-video");
-const { download_video } = require("./downloadVideo")
+const { program } = require("commander");
+const { exec } = require("youtube-dl-exec");
+const colors = require("colors");
 
-const program = new Command();
-
-console.log("My CLI tool is here!");
+program.version("1.0.0").description("A CLI tool to download YouTube videos");
 
 program
-  .name("yt-converter")
-  .description("CLI to some JavaScript string utilities")
-  .version("0.8.0");
-
-program
-  .command("hello <name>")
-  .option("-c, --capitalize", "Capitalize the name")
-  .description("Say hello to someone")
-  .action((name, options) => {
-    console.log(`Hello ${options.capitalize ? name.toUpperCase() : name}`);
-  });
-
-program
-   .command("download <videoUrl>")
-   .description('Download a video from YouTube: this command is not functioning for now due to changes in YouTube codes.')
-   .option('-f, --folder <folderName>', 'Output folder name', 'videos')
-   .option('-n, --name <fileName>', 'Output file name', 'video-file')
-   .action(async (videoUrl, options) => {
-    const { folder: folderName, name: fileName } = options;
-    try {
-        // const outputPath = await downloadVideo(videoUrl, folderName, fileName);
-        const outputPath = await download_video(videoUrl, folderName);
-        console.log(`Video downloaded to ${outputPath}`);
-    } catch (error) {
-        console.error(error);
+  .command("download <url>")
+  .option("-o, --output <path>", "Output directory and filename")
+  .action((url, options) => {
+    const { output } = options;
+    if (!url) {
+      console.error("Error: You must provide a YouTube video URL.".red);
+      process.exit(1);
     }
-   })
 
-program.parse(process.argv);
+    console.log(output);
+
+    if (!output) {
+      console.error("Error: You must provide a directory(folder and file name) for storing the video.".red);
+      process.exit(1);
+    }
+
+    console.log("Downloading video...".green);
+
+    exec(url, output)
+      .then((output) => {
+        console.log("Download complete!".green);
+        console.log(output);
+      })
+      .catch((error) => {
+        console.error(`Error: ${error.message}`.red);
+        process.exit(1);
+      });
+  });
